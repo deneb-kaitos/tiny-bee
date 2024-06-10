f498a3b54390acfc356feb235a84ae7aa28f57ac
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2024-06-10 16:19:01 +0200

    wip

--- sources/front-end/auth.app/src/routes/+page.svelte
+++ sources/front-end/auth.app/src/routes/+page.svelte
@@ -4,30 +4,96 @@
 </script>
 
 <style>
-  .main-article {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-auto-rows: 1fr;
-    grid-template-areas:
-      'main-visuals'
-      'vise-selector'
-    ;
+  .authentication-article {
+    display: block;
 
-    & .main-visuals-container {
-      grid-area: main-visuals;
-    }
+    & #authentication-form {
+      --form-width: 800px;
+      --form-height: 600px;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-auto-rows: 1fr;
+        grid-template-areas:
+          'legend-row'
+          'login-row'
+          'password-row'
+          'pin-row'
+          'submit-button-row'
+        ;
 
-    & .vise-selector-container {
-      grid-area: vise-selector;
-    }
+      gap: 0.5rem;
+
+      padding: 0.5rem;
+      position: relative;
+      top: calc(50% + var(--form-height) /  2);
+      margin: 0 auto;
+
+      width: var(--form-width);
+      /* height: var(--form-height); */
+      background-color: yellowgreen;
+        
+        & .form-row {
+          display: grid;
+          grid-template-columns: 1fr;
+          grid-template-rows: auto 1fr;
+          grid-template-areas:
+            'label'
+            'control'
+          ;
+          gap: calc(var(--main-grid-gap) / 2);
+          padding: 0.25rem;
+          background-color: brown;
+        }
+
+        & :is(label) { 
+          grid-area: label;
+          font-size: calc(var(--main-font-size) * 0.9);
+        }
+
+        & :is(input, button) {
+          grid-area: control;
+        }
+        
+        & .legend-row {
+          grid-area: legend-row;
+        }
+
+        & .login-row {
+          grid-area: login-row;
+        }
+
+        & .password-row {
+          grid-area: password-row;
+        }
+
+        & .pin-row {
+          grid-area: pin-row;
+        }
+
+        & .submit-button-row {
+          grid-area: submit-button-row;
+        }
+      }
   }
 </style>
 
-<article class="main-article">
-  <section class="main-visuals-container">
-    <MainVisual />
-  </section>
-  <section class="vise-selector-container">
-    <ViseSelector />
-  </section>
+<article class="authentication-article">
+  <form id="authentication-form">
+      <div class="legend-row">bla-bla-bla</div>
+      <div class="form-row login-row">
+        <label for="login">login</label>
+        <input id="login" type="text" />
+      </div>
+      <div class="form-row password-row">
+        <label for="password">password</label>
+        <input id="password" type="password" />
+      </div>
+      <div class="form-row pin-row">
+        <label for="pin">pin</label>
+        <input id="pin" type="text" />
+      </div>
+      <div class="form-row submit-button-row">
+        <button type="submit">ok</button>
+      </div>
+  </form>
 </article>
