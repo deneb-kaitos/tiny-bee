06def70c2450ec7a078be81208881a2f01a4e4fe
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2024-07-04 10:00:10 +0200

    wip: AuthZ && upgraded deps

--- sources/front-end/main.app/src/lib/controls/AuthZ/AuthZ.svelte
+++ sources/front-end/main.app/src/lib/controls/AuthZ/AuthZ.svelte
@@ -6,10 +6,45 @@
     signIn,
   });
   let activeTab = $state(signUp);
+  let login = $state(null);
+  let password = $state(null);
+  let pin = $state(null);
+
+  const credentials = {
+    login: null,
+    password: null,
+    pin: null,
+  };
+
+  $effect(() => {
+    login;
+    password;
+    pin;
+
+    credentials.login = login;
+    credentials.password = password;
+    credentials.pin = pin;
+
+    console.debug({ credentials });
+  });
 
   $effect(() => {
     console.debug(`activeTab: ${activeTab}`);
   });
+  
+  /**
+   * 
+   * @param {SubmitEvent} e
+   */
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    e.stopPropagation();
+
+    console.debug(e);
+
+    return false;
+  }
 </script>
 
 <style>
@@ -43,11 +78,18 @@
           padding: var(--main-padding);
           border-radius: var(--main-border-radius);
           color: var(--theme-white);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          font-size: calc(var(--main-font-size) / 2);
+          aspect-ratio: var(--main-aspect-ratio);
+          width: 2.5svw;
         }
 
         & > input[type='radio']:checked ~ label {
-          background-color: var(--theme-green);
-          color: var(--theme-black);
+          background-color: var(--theme-blue);
+          color: var(--theme-darkest_white);
         }
 
         & > input[type='radio'] {
@@ -61,7 +103,7 @@
       grid-template-columns: 1fr;
       grid-auto-flow: row;
       grid-auto-rows: 1fr;
-      gap: var(--main-grid-gap);
+      gap: calc(var(--main-grid-gap) * 2.5);
 
       & > .tab-panel-content-row {
         display: grid;
@@ -76,7 +118,6 @@
           cursor: pointer;
 
           aspect-ratio: var(--main-aspect-ratio);
-          min-width: min(5svw, 5svh);
         }
 
         & > input[type='text'],
@@ -94,6 +135,10 @@
           filter: opacity(0.5);
         }
 
+        & > input::placeholder {
+          color: var(--theme-light_gray_bright);
+        }
+
         & > button {
           border-radius: var(--main-border-radius);
           background-color: var(--theme-green);
@@ -102,7 +147,9 @@
         }
 
         & > label {
+          font-size: calc(var(--main-font-size) * 0.65);
           color: var(--theme-white);
+          padding-left: 0.8rem;
         }
       }
     }
@@ -124,24 +171,39 @@
           </li>
         {/each}
       </ul>
-      <form class='tab-panel-content'>
+      <form class='tab-panel-content' onsubmit={handleFormSubmit}>
         <div class='tab-panel-content-row'>
           <label for='input-login'>login</label>
-          <input id='input-login' type='text' placeholder='login' autocomplete='username' required />
+          <input
+            id='input-login'
+            type='text'
+            placeholder={ activeTab === signUp ? 'new login': 'your login' }
+            autocomplete='username'
+            required
+            bind:value={login}
+          />
         </div>
         <div class='tab-panel-content-row'>
           <label for='input-password'>password</label>
           <input
             id='input-password'
             type='password'
-            placeholder='password'
+            placeholder={ activeTab === signUp ? 'new password' : 'your password' }
             autocomplete={ activeTab === signUp ? 'new-password' : 'current-password' }
             required
+            bind:value={password}
           />
         </div>
         <div class='tab-panel-content-row'>
           <label for='input-pin'>pin</label>
-          <input id='input-pin' type="text" placeholder='pin' autocomplete='one-time-code' disabled={activeTab === signIn} />
+          <input
+            id='input-pin'
+            type='text'
+            placeholder='pin'
+            autocomplete='one-time-code'
+            disabled={activeTab === signIn}
+            bind:value={pin}
+          />
         </div>
         <div class='tab-panel-content-row'>
           <button type='submit'>
