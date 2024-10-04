601646194aecae0c340b7a4d3858e4f30c8e4d91
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2024-10-04 17:39:59 +0200

    enter page

--- sources/front-end/main.app/src/lib/controls/Enter/Enter.svelte
+++ sources/front-end/main.app/src/lib/controls/Enter/Enter.svelte
@@ -1,37 +1,62 @@
 <script>
-  import RegisterCtrl from "../RegisterCtrl/RegisterCtrl.svelte";
-  import LoginCtrl from "../LoginCtrl/LoginCtrl.svelte";
+  import AuthZForm from "$lib/controls/AuthZForm/AuthZForm.svelte";
+  import { AuthZMode } from "$lib/controls/AuthZForm/AuthZMode.svelte.js";
+
+  /**
+   * 
+   * @param {Object | null | undefined} fields
+   */
+  const handleOnValues = (fields = null) => {
+    console.debug({ fields });
+  };
 </script>
 
 <style>
   .enter {
+    --register-bg-color: var(--theme-green);
+    --login-bg-color: var(--theme-glacier);
+
     display: grid;
     grid-template-areas:
       'register login'
     ;
     min-height: 100svh;
-    gap: var(--main-grid-gap);
+    background-color: var(--theme-none);
 
     & > .register {
       grid-area: register;
+      justify-items: end;
+      background-color: var(--register-bg-color);
     }
 
     & > .login {
       grid-area: login;
+      justify-items: start;
+      background-color: var(--login-bg-color);
     }
 
     & > :is(.register, .login) {
       display: grid;
-      place-items: center;
+      align-items: center;
+      container-type: inline-size;
+      padding: var(--main-padding);
     }
   }
 </style>
 
 <article class='enter'>
   <section class='register'>
-    <RegisterCtrl />
+    <AuthZForm
+      mode={AuthZMode.register}
+      formLegend="register account"
+      onValues={handleOnValues}
+    />
   </section>
   <section class='login'>
-    <LoginCtrl />
+    <AuthZForm
+      mode={AuthZMode.login}
+      formLegend="login"
+      onValues={handleOnValues}
+    />
   </section>
 </article>
